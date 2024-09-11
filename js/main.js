dd646a627a0c3a8be6d1059a16ccb01046db29c1
Christina <150205323+berryalien@users.noreply.github.com>
2024-09-11 23:36:43 +0400

    added styles for the banner-section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
 
+$('.banner-setion__slider').slick({
+  dots: true,
+  prevArrow: '<button class="banner-setion__slider-btn banner-setion__slider-btnprev" ><img src="images/content/arrow-left.svg" alt=""></button>',
+  nextArrow: '<button class="banner-setion__slider-btn banner-setion__slider-btnnext" ><img src="images/content/arrow-right.svg" alt=""></button>',
+});
+
 });
