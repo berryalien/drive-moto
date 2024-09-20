286725e3bcadcfb0b7a110ad392f90641e3b777c
Christina <150205323+berryalien@users.noreply.github.com>
2024-09-20 22:32:37 +0400

    added style for the search section

--- js/main.js
+++ js/main.js
@@ -6,4 +6,17 @@ $('.banner-setion__slider').slick({
   nextArrow: '<button class="banner-setion__slider-btn banner-setion__slider-btnnext" ><img src="images/content/arrow-right.svg" alt=""></button>',
 });
 
+
+
+$('.search__tabs-item').on('click', function(e){
+  e.preventDefault();
+
+$('.search__tabs-item').removeClass('search__tabs-item--active');
+$('.search__content-item').removeClass('search__content-item--active');
+
+$(this).addClass('search__tabs-item--active');
+$($(this).attr('href')).addClass('search__content-item--active');
+
+})
+
 });
