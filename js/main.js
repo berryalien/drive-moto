92ef171ba94956e7aeb326e93a3465443182bc40
Christina <150205323+berryalien@users.noreply.github.com>
2024-11-18 21:25:12 +0700

    added markup + styles for the catalog section

--- js/main.js
+++ js/main.js
@@ -21,10 +21,36 @@ $($(this).attr('href')).addClass('tabs-content--active');
 
 $('.filter-style').styler();
 
-$('.filter__item-drop').on('click', function(){
+$('.filter__item-drop, .filter__extra').on('click', function () {
   $(this).toggleClass('filter__item-drop--active');
   $(this).next().slideToggle('200');
 });
+
+
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 100000,
+  max: 500000,
+});
+
+
+$('.catalog__filter-btngrid ').on('click', function () {
+      $(this).addClass('catalog__filter-button--active');
+      $('.catalog__filter-btnline').removeClassClass('catalog__filter-button--active');
+      $('.product-item__wrapper').removeClassClass('product-item__wrapper--list');
+});
+
+$('.catalog__filter-btnline').on('click', function () {
+      $(this).addClass('catalog__filter-button--active');
+      $('.catalog__filter-btngrid').removeClassClass('catalog__filter-button--active');
+      $('.product-item__wrapper').addClass('product-item__wrapper--list');
+});
+
+
+
+
+
+
 
 
 });
